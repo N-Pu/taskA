ac83b0e88f9ef56f10c1b42b3d5c3654cb0113ca
Harry.G <garry.garyra@mail.ru>
2021-07-08 13:02:56 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,14 @@
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        SubClass subClass = new SubClass();
+        String input = "fds4jvd43";
+
+        String result = SubClass.getString(input);
+        System.out.println(result);
+        System.out.println();
+
+    }
+
+}
---
+++ src/SubClass.java
@@ -0,0 +1,40 @@
+public class SubClass {
+
+    public static String getString(CharSequence input) {
+        StringBuilder stringBuilder = new StringBuilder(input.length()); // длинна строки
+        for (int i = 0; i < input.length(); i++) {
+            char j = input.charAt(i);
+            char k = input.charAt(i);
+            if (64 < k && k < 123) {
+                stringBuilder.append(k);
+            }
+            if (j > 47 && j < 58) {
+                Object a = stringBuilder.append(j);
+                //a = Integer.parseInt(j);
+            }
+
+        }
+        return (stringBuilder.toString());
+    }
+
+    public void parsToInt(String j) {
+        int foo = Integer.parseInt(j);
+    }
+
+
+    public static int getNumber(CharSequence input, int a) {
+        StringBuilder stringBuilder2 = new StringBuilder(input.length());
+        for (int i = 0; i < input.length(); i++) {
+            char j = input.charAt(i);
+            if (j > 47 && j < 58) {
+                a = Integer.parseInt(String.valueOf(j));
+                break;
+
+            }
+        }
+
+        return a;
+    }
+
+
+}
