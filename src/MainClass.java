5afee724f6efccf3bbf61c6e7b6d58085d6e4275
Harry.G <garry.garyra@mail.ru>
2021-07-09 14:37:31 +0300

    Update typeA.

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,10 +4,12 @@ public class MainClass {
     public static void main(String[] args) {
         SubClass subClass = new SubClass();
         String input = "fds4jvd43";
-
+        subClass.parsToInt("4443");
         String result = SubClass.getString(input);
         System.out.println(result);
         System.out.println();
+        SubClass subClass1 = new SubClass();
+        subClass1.getNum(input, 1);
 
     }
 
--- src/SubClass.java
+++ src/SubClass.java
@@ -1,5 +1,21 @@
 public class SubClass {
 
+
+    public static int getNum(CharSequence input, int a) {
+        StringBuilder stringBuilder2 = new StringBuilder(input.length());
+        for (int i = 0; i < input.length(); i++) {
+            char j = input.charAt(i);
+            if (j > 47 && j < 58) {
+                a = Integer.parseInt(String.valueOf(j));
+                break;
+
+            }
+        }
+        System.out.println(a);
+        return a;
+    }
+
+
     public static String getString(CharSequence input) {
         StringBuilder stringBuilder = new StringBuilder(input.length()); // длинна строки
         for (int i = 0; i < input.length(); i++) {
@@ -22,19 +38,6 @@ public class SubClass {
     }
 
 
-    public static int getNumber(CharSequence input, int a) {
-        StringBuilder stringBuilder2 = new StringBuilder(input.length());
-        for (int i = 0; i < input.length(); i++) {
-            char j = input.charAt(i);
-            if (j > 47 && j < 58) {
-                a = Integer.parseInt(String.valueOf(j));
-                break;
-
-            }
-        }
-
-        return a;
-    }
 
 
 }
